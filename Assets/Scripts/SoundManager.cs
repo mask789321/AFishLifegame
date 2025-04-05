4ca426e1af2931cb5c94c048015a9a5339eb79a8
rodriguezanthony1104 <rodriguezanthony1104@gmail.com>
2025-04-05 13:37:46 -0400

    basic sound manager up + fixed* sliding

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class SoundManager : MonoBehaviour
+{   
+    //reference to access anywhere
+    public static SoundManager Instance;
+
+    public AudioSrc[] music, sfx;
+    public AudioSource musicSource, sfxSource;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayMusic("Acceptance");
+    }
+
+    //finds and plays music track
+    public void PlayMusic(string name)
+    {
+        AudioSrc s = Array.Find(music, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("Music not found.");
+        }
+
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+
+    }
+
+    //finds and plays sound effects
+    public void PlaySFX(string name)
+    {
+        AudioSrc s = Array.Find(sfx, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound not found.");
+        }
+
+        else
+        {
+            sfxSource.PlayOneShot(s.clip);
+        }
+
+    }
+
+}
