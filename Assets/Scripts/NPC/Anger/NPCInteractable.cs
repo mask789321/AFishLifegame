d2873f98bba0a091b58c82d4900d34725d84c09d
Cel Lewis <149333423+auxpatch@users.noreply.github.com>
2025-04-06 12:09:59 -0400

    Coroutine Dialogue
    set dialogue to a coroutine, when fish is interacted with, it activates the dialogue. this is just for prototype and will need to be much more specific later on

--- Assets/Scripts/NPC/Anger/NPCInteractable.cs
+++ Assets/Scripts/NPC/Anger/NPCInteractable.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class NPCInteractable : MonoBehaviour
 {
+    public GameObject FishUI;
+
+    public void Start()
+    {
+        FishUI.SetActive(false);
+    }
     public void Interact()
     {
         Debug.Log("Check this shit out");
+        FishUI.SetActive(true);
+        StartCoroutine(SetFalse());
     }
+
+    IEnumerator SetFalse()
+    {
+        yield return new WaitForSeconds(3);
+        FishUI.SetActive(false);
+    }
+
+
+
 }
