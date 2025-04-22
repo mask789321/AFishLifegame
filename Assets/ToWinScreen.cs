44068548de9208f7dd79fd809ad5331cbac9a489
BlueJz <122586682+BlueJz@users.noreply.github.com>
2025-04-22 15:49:10 -0400

    Game UI
    Main Menu, Win Screen

---
+++ Assets/ToWinScreen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToWinScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("WinScreen");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
